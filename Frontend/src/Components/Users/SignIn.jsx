9d1993afce8f69dbcefced3337b1f5c32163f295
Rohit Lohra <78945252+Rohit9113@users.noreply.github.com>
2024-12-02 11:17:32 +0530

    Normal Commit

--- Frontend/src/Components/Users/SignIn.jsx
+++ Frontend/src/Components/Users/SignIn.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div className='bg-black w-full h-96'>SignIn</div>
+    <div className='bg-black md:w-full md:h-[633px] flex flex-wrap gap-[32px] mx-auto items-center justify-center'>
+        <div className='md:w-[707px] md:h-[533px] bg-white'>
+            {/* Image and Text Dev */}
+            <p className='text-start text-[32px]'>Explore your hobby or passion</p>
+        </div>
+
+        <div className='bg-white md:w-[410px] md:h-[533px]'>
+            {/* SignIn div */}
+
+        </div>
+    </div>
   )
 }
